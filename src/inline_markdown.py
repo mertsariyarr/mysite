a5d03cec971054c091bac54f5dee8dcc4e89e529
mertsariyar <mertsariyar2@gmail.com>
2025-04-27 14:57:05 +0300

    created textnodetoall method and unittest

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -95,6 +95,21 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
-print(split_nodes_link([node]))
+
   
 
+
+myText = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "'", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
+
+print(text_to_textnodes(myText))
